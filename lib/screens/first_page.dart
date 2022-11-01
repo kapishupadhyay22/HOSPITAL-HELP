0ac1d631c9be06202858b9da9c3f53f447580479
kapishupadhyay22 <92024461+kapishupadhyay22@users.noreply.github.com>
2022-11-01 23:52:47 +0530

    designing of firstpage

--- lib/screens/first_page.dart
+++ lib/screens/first_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hospital_auth/screens/singin_screen.dart';
-import '../main.dart';
+import 'package:hospital_auth/utils/color_utils.dart';
+import 'package:hospital_auth/main.dart';
 
 class first_page extends StatefulWidget {
   const first_page({Key? key}) : super(key: key);
@@ -19,54 +20,97 @@ class _first_pageState extends State<first_page> {
           elevation: 0,
           title: const Text(
             "HOSPITAL HELP",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
           ),
         ),
-        body: Center(
-            child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 250,
-            ),
-            const Text(
-              "Sign in as -",
-              style: TextStyle(fontSize: 30),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                child: const Text("USER"),
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50))),
-                onPressed: () {
-                  a = 1;
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SignInScreen()));
-                },
-              ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  child: const Text("ADMIN"),
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50))),
-                  onPressed: () {
-                    a = 2;
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignInScreen()));
-                  }),
-            ),
-          ],
-        )));
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              hexStringToColor("416C71"),
+              hexStringToColor("7e253e"),
+              hexStringToColor("5c5398")
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            child: SingleChildScrollView(
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      20, MediaQuery.of(context).size.height * 0.3, 20, 0),
+                  child: Column(
+                    children: [
+                      const Text("Sigin as a ?",
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                a = 1;
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const SignInScreen()));
+                              },
+                              child: const Text(
+                                "USER",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.resolveWith(
+                                          (states) {
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.black26;
+                                    }
+                                    return Colors.white;
+                                  }),
+                                  shape: MaterialStateProperty.all(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30)))))),
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                a = 2;
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const SignInScreen()));
+                              },
+                              child: const Text(
+                                "ADMIN",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.resolveWith(
+                                          (states) {
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.black26;
+                                    }
+                                    return Colors.white;
+                                  }),
+                                  shape: MaterialStateProperty.all(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30))))))
+                    ],
+                  )),
+            )));
   }
 }
