0fc4fc6fa32179c67b85524c7cc5d90bce80ae61
hammad khan <hammad2000khan6528@gmail.com>
2022-11-08 20:57:32 +0530

    1

--- lib/screens/pfp.dart
+++ lib/screens/pfp.dart
@@ -2,18 +2,131 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class profileScreen extends StatefulWidget {
-  const profileScreen({super.key});
+class MyProfile extends StatelessWidget {
+  const MyProfile({Key? key}) : super(key: key);
 
   @override
-  State<profileScreen> createState() => _profileScreenState();
-}
+  Widget listTile({icon, title}) {
+    return Column(
+      children: [
+        Divider(
+          height: 0.5,
+        ),
+        ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          trailing: Icon(Icons.arrow_forward_ios),
+        ),
+      ],
+    );
+  }
 
-class _profileScreenState extends State<profileScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.orangeAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        elevation: 0.0,
+        title: Text(
+          "Your Profile",
+          style: TextStyle(fontSize: 25, color: Colors.black),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 100,
+                color: Colors.orangeAccent,
+              ),
+              Container(
+                height: 561,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 15,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30)),
+                ),
+                child: ListView(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 250,
+                          height: 80,
+                          padding: EdgeInsets.only(left: 5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: 20),
+                                  Text(
+                                    "Name:",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 18,
+                                        color: Colors.black),
+                                  ),
+                                  SizedBox(height: 15),
+                                  Text("Email:",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 18))
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListTile(
+                        title: Text("🩸 Blood Group :"),
+                        subtitle: Text("    q")),
+                    ListTile(
+                        title: Text("🚻 Gender:"), subtitle: Text("    q")),
+                    ListTile(
+                        title: Text("𐄷 Weight:"), subtitle: Text("    q")),
+                    ListTile(title: Text("〖 Height:"), subtitle: Text("    q")),
+                    ListTile(title: Text("Booking Status:")),
+                    ListTile(
+                        title: Text(
+                      " LOG OUT 🛅",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+                    )),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 30,
+              left: 25,
+            ),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/Unknown.png"),
+                radius: 45,
+                backgroundColor: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
