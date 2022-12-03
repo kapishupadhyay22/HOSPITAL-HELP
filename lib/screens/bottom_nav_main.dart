d3198bf2c8f3bc5d2cbd4c67d3a11d6f4cb99bcf
kapishupadhyay22 <92024461+kapishupadhyay22@users.noreply.github.com>
2022-12-03 19:57:18 +0530

    dhb

--- lib/screens/bottom_nav_main.dart
+++ lib/screens/bottom_nav_main.dart
@@ -16,7 +16,6 @@ class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: screen[currentIndex],
-        resizeToAvoidBottomInset: false,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: Container(
           margin: const EdgeInsets.fromLTRB(0, 90, 0, 0),
